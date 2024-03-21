2585032d0f12186f442fbebef2d313970becbbbd
Robrecht Cannoodt <rcannood@gmail.com>
2024-03-21 12:20:48 +0100

    update scripts & add placeholder nf and config

--- scripts/run_local_with_remote.sh
+++ scripts/run_local_with_remote.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-OUT=output/local
+OUT=output/local_with_remote
 
 # clear output dir
 rm -r "$OUT"
 
 # run component
 NXF_VER=23.10.0 nextflow run \
-  . \
+  viash-io/test-nextflow-wave-fusion \
+  -r main_build \
   -main-script target/nextflow/method/main.nf \
   -profile docker \
   --input resources/input1.txt \
