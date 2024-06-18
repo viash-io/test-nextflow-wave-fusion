f3d714056d65eff6fa1c465de1f4b4aa9ce2e1b8
dorien-er <dorien-er@users.noreply.github.com>
2024-06-18 14:01:24 +0000

    deploy: ee70d55267781e531d3273c4d53300dd3c5ec623

--- scripts/run_tw-code_remote-input_s3-output_s3-work_s3_nxf23101_vsh085.sh
+++ scripts/run_tw-code_remote-input_s3-output_s3-work_s3_nxf23101_vsh085.sh
@@ -3,16 +3,19 @@
 set -e
 
 script_name=$(basename "$0")
-TMPOUT=s3://data-intuitive-tmp/test-nextflow-wave-fusion/output/${script_name%.sh}
+BUCKET=""  # REPLACE
+TMPOUT=$BUCKET/test-nextflow-wave-fusion/output/${script_name%.sh}
 OUT=output/${script_name%.sh}
-RES=s3://data-intuitive-tmp/test-nextflow-wave-fusion/resources
-WORK=s3://data-intuitive-tmp/test-nextflow-wave-fusion/work/${script_name%.sh}
+RES=$BUCKET/test-nextflow-wave-fusion
+WORK=$BUCKET/test-nextflow-wave-fusion/work/${script_name%.sh}
 NXF_CONFIG=/tmp/${script_name%.sh}.config
 PARAMS=/tmp/${script_name%.sh}.yaml
+WORKSPACE=""  # REPLACE
+COMPUTE_ENV=""  # REPLACE
+TOWER_API_ENDPOINT="" # REPLACE
 
-# set aws profile
-export AWS_PROFILE=di
-echo "aws profile: $AWS_PROFILE"
+export TOWER_ACCESS_TOKEN=""  # REPLACE
+export TOWER_API_ENDPOINT=""  # REPLACE
 
 # clear output dir
 echo "Clearing output directory"
@@ -21,19 +24,10 @@ aws s3 rm $TMPOUT --recursive
 
 # run component
 echo "Running component"
-
 cat > $NXF_CONFIG <<EOF
-process {
-    executor = 'awsbatch'
-    queue = 'nextflow_queue_nofusion'
-    container = 'bash:4.0'
-}
-
-aws {
-    region = 'eu-west-1'
-    batch {
-        cliPath = '/home/ec2-user/miniconda/bin/aws'
-    }
+tower {
+    enabled = true
+    endpoint = '$TOWER_API_ENDPOINT'   
 }
 EOF
 
@@ -44,19 +38,19 @@ publish_dir: "$TMPOUT"
 EOF
 
 tw launch \
-  viash-io/test-nextflow-wave-fusion \
-  --revision main_build \
+  test-nextflow-wave-fusion \
+  --revision build_0_8_5 \
   --main-script target/nextflow/method/main.nf \
   --work-dir $WORK \
-  --workspace XXX \
-  --compute-env YYY \
+  --workspace $WORKSPACE \
+  --compute-env $COMPUTE_ENV \
   --config $NXF_CONFIG \
   --params-file $PARAMS
 
 # sync output
 echo "Syncing output"
-aws s3 sync $TMPOUT $OUT
+echo "aws s3 sync $TMPOUT $OUT"
 
 # check if output is correct
 echo "Checking output"
-scripts/verify_output.sh "$OUT"
+echo "scripts/verify_output.sh $OUT"
